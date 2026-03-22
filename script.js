c715ff3332645aa60ef92b8f4e6c31329a903806
Aarya Niraula <aaryaniraula5@gmail.com>
2026-03-22 08:19:22 +0545

    jquery

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-function calculate(){
+function calculate(operation){
 
-let m1 = Number(document.getElementById("m1").value);
-let m2 = Number(document.getElementById("m2").value);
-let m3 = Number(document.getElementById("m3").value);
-let m4 = Number(document.getElementById("m4").value);
-let m5 = Number(document.getElementById("m5").value);
-let m6 = Number(document.getElementById("m6").value);
-let m7 = Number(document.getElementById("m7").value);
-let m8 = Number(document.getElementById("m8").value);
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+    let result;
 
-let total = m1+m2+m3+m4+m5+m6+m7+m8;
+    if (isNaN(n1) || isNaN(n2)) {
+        result = "Please enter valid numbers";
+    }
+    else if (n1 > 100 || n2 > 100) {
+        result = "Number should not be greater than 100";
+    }
+    else if(operation === "add"){
+        result = n1 + n2;
+    }
+    else if(operation === "sub"){
+        result = n1 - n2;
+    }
+    else if(operation === "mul"){
+        result = n1 * n2;
+    }
+    else if(operation === "div"){
+        if(n2 === 0){
+            result = "Cannot divide by zero";
+        } else {
+            result = n1 / n2;
+        }
+    }
+    else{
+        result = "Invalid operation";
+    }
 
-let result = document.getElementById("result");
-
-if(total < 400){
-    result.innerHTML = "Total: " + total + " - Fail";
-    result.style.color = "red";
-}
-else if(total < 500){
-    result.innerHTML = "Total: " + total + " - Third Division";
-}
-else if(total < 600){
-    result.innerHTML = "Total: " + total + " - Second Division";
-}
-else if(total < 700){
-    result.innerHTML = "Total: " + total + " - First Division";
-}
-else{
-    result.innerHTML = "Total: " + total + " - Distinction";
-    result.style.color = "green";
-}
-
+    document.getElementById("result").innerText = "Result: " + result;
 }
